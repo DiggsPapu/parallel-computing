d167b14da22a1a750557d30539da791535165743
DiggsPapu <diggs.papu@gmail.com>
2024-09-25 14:59:30 -0600

    feat: All exercises done

--- mpi_hello_2.c
+++ mpi_hello_2.c
@@ -14,6 +14,9 @@ int main(int argc, char** argv) {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+    // Imprimir el mensaje desde cada proceso
+    printf("Hello World, I'm process %d of %d!\n", world_rank, world_size);
+    
     // Buffer para el mensaje
     char message[100];
 
