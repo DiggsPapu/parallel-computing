e15cbe89198236ee27a6d30b3a96baef01ea7c7f
DiggsPapu <diggs.papu@gmail.com>
2024-10-02 19:48:40 -0600

    add: Threshold version

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Parallel Computing
 
 Diego Andrés Alonzo Medinilla 20172
+
+
+## Al realizar la paralelización, ¿qué está pasando internamente y por qué el resultado no mejora
+
+En primer lugar open mp crea y gestiona muchas tareas y esto se sobrecarga, de manera que a pesar que se creen en diferentes hilos estas se deben de gestionar, distribuirlas entre hilos y sincronizarlas a partir del taskwait. Esto es producto por ser tareas muy pequeñas ya que solo es sumas y demás lo que precisamente introduce una sobrecarga por gestión de hilos.
+
--- fib.c
+++ fib.c
@@ -28,6 +28,30 @@ long long fib_recursive(int n) {
     return fib_recursive(n - 1) + fib_recursive(n - 2);
 }
 
+#define THRESHOLD 20  // Definir un umbral para evitar la creación de tareas innecesarias
+
+// Función recursiva paralelizada con OpenMP y threshold
+long long fib_recursive_omp_fix(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    // Si el valor de n es menor que el umbral, calcular secuencialmente
+    if (n < THRESHOLD) {
+        return fib_recursive(n);
+    }
+
+    long long x, y;
+
+    #pragma omp task shared(x)
+    x = fib_recursive_omp_fix(n - 1);
+
+    #pragma omp task shared(y)
+    y = fib_recursive_omp_fix(n - 2);
+
+    #pragma omp taskwait
+    return x + y;
+}
+
 int main() {
     int n;
     long long result;
@@ -59,6 +83,19 @@ int main() {
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Fibonacci de %d es: %lld (con OpenMP)\n", n, result);
     printf("Tiempo de ejecución (con OpenMP): %f segundos\n", cpu_time_used);
+
+     // Registrar el tiempo de inicio para la versión con OpenMP y threshold
+    start = clock();
+    #pragma omp parallel
+    {
+        #pragma omp single
+        result = fib_recursive_omp_fix(n);
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Fibonacci de %d es: %lld (con OpenMP y threshold)\n", n, result);
+    printf("Tiempo de ejecución (con OpenMP y threshold): %f segundos\n", cpu_time_used);
 
 
     return 0;
